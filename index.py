1c52338005d2c89301da21f00e3cef87470cea17
Adhithyaa1 <65454655+Adhithyaa1@users.noreply.github.com>
2020-10-04 03:46:58 +0530

    Update index.py

--- index.py
+++ index.py
@@ -65,9 +65,9 @@ def split_filter_part(filter_part):
 
 
 @app.callback(
-    Output('table-sorting-filtering', 'data'),
-    Output('qty' , 'children')
-     , [Input('table-sorting-filtering', "page_current")
+    [Output('table-sorting-filtering', 'data'),
+    Output('qty' , 'children')],
+      [Input('table-sorting-filtering', "page_current")
      , Input('table-sorting-filtering', "page_size")
      , Input('table-sorting-filtering', 'sort_by')
      , Input('table-sorting-filtering', 'filter_query')
